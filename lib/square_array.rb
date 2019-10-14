cb20669402f32dc18f7a201d0612bb89e2800268
remi.odufuye <remi.odufuye@gmail.com>
2019-10-14 14:38:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,19 +1,15 @@
 
-
-#  def square_array(array)
- 
+# Solution Below did not work so tried a different approach 
+#   def square_array(array)
 #   counter = 0
-#  new_array = []
- 
+#   new_array = []
 #   while counter < array.length do
 #   square_value = array[counter] * array[counter]
 #   new_array << square_value
 #   counter += 1
 #   end
-  
- # p square_value
- 
-# end
+#   p square_value
+#   end
 
 
 def square_array(ary)
