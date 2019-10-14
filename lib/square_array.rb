971084832f68a459356d04a8884ebadb7adf3b7b
remi.odufuye <remi.odufuye@gmail.com>
2019-10-14 14:34:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,17 @@
+
+
 def square_array(array)
-  # your code here
+ 
+   counter = 0
+   new_array = []
+ 
+  while counter < array.length do
+    square_value = array[counter] * array[counter]
+    new_array << square_value
+    counter += 1
+  end
+  
+  p square_value
+ 
 end
+
