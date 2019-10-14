aaedfe217c680a32fc462e92fdedc69a5760838e
remi.odufuye <remi.odufuye@gmail.com>
2019-10-14 14:38:40 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,19 @@
-def square_array(array)
-  # your code here
+
+# Solution Below did not work so tried a different approach 
+#   def square_array(array)
+#   counter = 0
+#   new_array = []
+#   while counter < array.length do
+#   square_value = array[counter] * array[counter]
+#   new_array << square_value
+#   counter += 1
+#   end
+#   p square_value
+#   end
+
+
+def square_array(ary)
+  a = Array.new(ary.length)
+  ary.each_index { |i| a[i] = ary[i] * ary[i] }
+  a
 end
